e49c01c2502a56c31dfca6a2586d59b588cb7c0c
rudwheel <59952660+rudwheel@users.noreply.github.com>
2023-03-17 17:43:17 +0100

    feat: Ajout de l'autorisation des CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('API TRIPPPY')
     .setDescription('TRIPPPY Back end Application ')
